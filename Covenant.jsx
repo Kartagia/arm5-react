5068c5c20a600561247396055a11e788f8d3bb20
Antti Kautiainen <kautsu@elisanet.fi>
2023-09-01 10:25:00 +0300

    feat: defaultProps and propTypes * fix: removed unused imports. * refactor: moved Covenant constructor to the modules.covenant.js * refactor: changed the in file name of the default import to   CovenantComponent * Added prop types ot the CovenantComponent * Added default prop types of the CovenantComponent

--- Covenant.jsx
+++ Covenant.jsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import { getTribunals } from './arm5.js';
-import { getArts } from './modules.art.js';
+import React from "react";
+import PropertyTypes from "prop-types";
 import History from "./History.jsx";
+import Covenant from "./modules.covenant.js";
 
-export function Covenant(props) {
-  
-}
+/**
+ * The covenant component properties.
+ * @typedef {React.Props} CovenantProps
+ * @property {Covenant} covenant The coveannt the
+ */
 
-
-export default function Covenant(props) {
+/**
+ *
+ * @param {CovenantProp} props The covenant properties.
+ * @returns
+ */
+export default function CovenantComponent(props) {
   const covenant = props.covenant;
-  
-  // Create result
-  return (
-    <React.Fragment><h1>Covenant of {covenant.name}</h1>
-    {props.children}
-    <History>
-    <p>Test child paragraph</p>
-    </History>
-    </React.Fragment>
+  if (covenant instanceof Covenant) {
+    // The compoent is created with a covenant ifnormation.
+    // Create result
+    return (
+      <React.Fragment>
+        <h1>Covenant of {covenant.name}</h1>
+        {props.children}
+        <History entries={covenant.history ? covenant.history : []} />
+      </React.Fragment>
     );
+  } else {
+    // Create a new covenant
+    return (
+      <React.Fragment>
+        <form></form>
+      </React.Fragment>
+    );
+  }
 }
+
+CovenantComponent.defaultProps = {};
+
+CovenantComponent.propTypes = {
+  covenant: PropertyTypes.object,
+  children: PropertyTypes.any,
+};
