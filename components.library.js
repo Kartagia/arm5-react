ff25046e50177dfe2fb5921b496ae83edd251860
kautsu@elisanet.fi <kautsu@elisanet.fi>
2023-10-24 01:15:20 +0300

    feat: updsted component library\n * parseContent, Content, and QualityAndLeveö works properly

--- components.library.js
+++ components.library.js
@@ -5,43 +5,51 @@ import {useState} from 'react';
  * @module components.librsry
  */
  
- export function QualityAndLevel({quality=null, level=null}) {
-   if (quality != null && level != null) {
-     return <span><span className={level}>{level}</span>/<span className="quality">{quality}</span></span>;
-     
-   } else if (quality != null) {
-     return <span className="quality">{quality}</span>;
-   } else {
-     return <span />;
-   }
- }
- 
+ export function QualityAndLevel({ quality = null, level = null }) {
+  if (quality != null && level != null) {
+    return <span><span className={level}>{level}</span>/<span className="quality">{quality}</span></span>;
+
+  } else if (quality != null) {
+    return <span className="quality">{quality}</span>;
+  } else {
+    return <span />;
+  }
+}
+
+
+export function Content(props) {
+
+  return (<article className="content" ><span className="title"><em>{props.title || "(unknown)"}</em> by <em>{props.author || "(unknown)"}</em></span><span>{props.target}</span><span>{props.type}</span>
+  <QualityAndLevel level={props.level} quality={props.quality} /></article>);
+}
+
+export function parseContents(contents = [], defaultValues = {}) {
+  console.group(`Parse contents`);
+  console.table([contents, defaultValues])
+  console.groupEnd();
+  const result = contents.map(
+    (contentDef, index) => {
+      return {
+      id: (contentDef.id || `content-${index}`),
+      title: (contentDef.title || defaultValues.title || "(unknown)"),
+      author: (contentDef.author || defaultValues.author || "(unknown)"),
+      level: contentDef.level,
+      quality: contentDef.quality,
+      lang: (contentDef.lang || defaultValues.lang || "Latin"),
+      script: (contentDef.script || defaultValues || "Latin"),
+      targetType: (contentDef.targetType || defaultValues.targetType || "Anility"),
+      target: contentDef.target || defaultValues.target,
+      type: (contentDef.type || defaultValues.type  || "Tractatus")
+    }});
+    
+    console.groupEnd();
+    return result;
+}
  
  export function Summary(props) {
     return (<div className="summary" >{(props.lang || "Latin")}/{(props.script || "Latin")}. {props.targetType} {props.target} {props.type}<QualityAndLevel level={props.level} quality={props.quality} /></div>);
  }
  
- 
- 
- export function Content(props) {
-   
-   return (<article className="content" ><span className="title"><em>{props.title}</em> by <em>{props.author || "unknown"}</em></span><span>{props.targetType}</span><span>{props.type}</span><span>{props.target}</span></article>);
- }
- 
- export function parseContents(contents=[]) {
-   return contents.map(
-     (contentDef, index) => ({
-       id: (contentDef.id || `content-${index}`),
-       title: (contentDef.title ? `"${contentDef.title}"` : "unknown"),
-       level: contentDef.level,
-       quality: contentDef.quality,
-       lang: (props.lang || "Latin"),
-       script: (props.script || "Latin"),
-       targetType: (props.targetType || "Anility"),
-       target: contentDef.target,
-       type: (contentDef.type || "Tractatus")
-     }));
- }
  
  export function Book(props) {
    const [status, setStatus] = useState([(props.status || "Available"), (props.location || "Public collection")]);
