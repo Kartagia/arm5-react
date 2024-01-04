e04303444328a4ca2dad443ded13f4cad3045300
Antti Kautiainen <antti@kautiainen.com>
2024-01-05 01:11:00 +0200

    modified:   model.skills.mjs - BREAKING CHANGE: SkillGroupModel constructor   - Changing title and name. The name is determined from the title     by removing the parentheses of the placeholder. - feat: constant defaultSkillType stores the default skill type. - feat: pojo SkillGroupModel   - Added property "skillTypes" which contains the default skill types     of the group members.   - Added the skill types support - feat: pojo SkillModel   - Added property "skillTypes" which contains all skill types of the     skill.   - Added the skill types to the constructor of a skill model. - feat: class SkillGroupMember   - Added skill types support by taking member types before group types,     and defaulting to the default skill type.

--- model.skills.mjs
+++ model.skills.mjs
@@ -1,7 +1,8 @@
-
 /**
  * @module skills
  */
+
+import { ReferenceLibrary } from "./modules.referencelibrary.mjs";
 
 /**
  * A function converting a value into a string.
@@ -18,6 +19,7 @@
  * @property {string} [title] The title of the skill. Defaults to the
  * name.
  * @property {SkillGroupModel} [group] The group of the skill.
+ * @property {string[]} [skillTypes] The skill types of the skill model.
  * @property {string} [abbreviation] The abbreviation of the skill.
  * @property {string} [description] The description of the skill.
  */
@@ -25,13 +27,22 @@
 /**
  * Create a new skill.
  * @param {string} skillName The name of the skill.
- * @param {string} [title] The title of the skill. Defaults to the name. 
+ * @param {string} [title] The title of the skill. Defaults to the name.
  * @param {string} [abbreviation] The abbreviation of the skill.
- * @param {string} [description] The description of the skill. 
+ * @param {string} [description] The description of the skill.
+ * @param {string[]} [skillTypes] The list of group names. Defaults to the
+ * skill types of the group, or ["General"] if group and skill types is undefined.
  * @param {SkillGroupModel} [group] The group of the skill.
- * @returns {SkillModel} The skill model. 
+ * @returns {SkillModel} The skill model.
  */
-export function createSkill(skillName, title=undefined, abbreviation=undefined, description=undefined, group=undefined) {
+export function createSkill(
+  skillName,
+  title = undefined,
+  abbreviation = undefined,
+  description = undefined,
+  group = undefined,
+  skillTypes = [],
+) {
   if (!validSkillName(skillName)) {
     throw new TypeError("Invalid skill name");
   }
@@ -46,10 +57,11 @@ export function createSkill(skillName, title=undefined, abbreviation=undefined, 
     title,
     group,
     abbreviation,
-    description
+    description,
+    skillTypes: (skillTypes ? [...skillTypes] : (group ? [...(group.skillTypes)] : 
+    ["General"] ) )
   };
 }
-
 
 /**
  * @typedef {Object} SkillGroupModel
@@ -58,6 +70,8 @@ export function createSkill(skillName, title=undefined, abbreviation=undefined, 
  * name.
  * @property {string?} [abbreviation] The abbreviation of the skill.
  * @property {string} [description] The description of the skill.
+ * @property {string[]} [skillTypes] The list of default skill types of the 
+ * group members.
  * @property {ToStringer<SkillModel>} [composeMemberName] The composer of the
  * member name. Defaults to the function replacing the placeholder of the title
  * with the title of the skill.
@@ -65,7 +79,6 @@ export function createSkill(skillName, title=undefined, abbreviation=undefined, 
  * the gropu member abbreviated title from the member. Defaults to the function
  * separating the abbreviation with the title of the member.
  */
-
 
 /**
  * Test validity of a name.
@@ -84,7 +97,7 @@ const validName = (name) => {
  * The regular expression matching to a single word.
  * @type {RegExp}
  */
-const wordRe = /\p{Lu}\p{Ll}*/ug;
+const wordRe = /\p{Lu}\p{Ll}*/gu;
 
 /**
  * Test validity of a skill name.
@@ -140,8 +153,6 @@ export const validSkillGroupName = (name) => {
   );
 };
 
-
-
 /**
  * Test validity of an abbreviation. An abbreviation is a valid abbreviation, if it contains
  * upper case lettters.
@@ -155,15 +166,29 @@ export const validAbbreviation = (abbreviation) => {
   );
 };
 
+/**
+ * Test gropu member abbreviation.
+ * @param {string} abbreviation The tested abbreviation.
+ * @returns {boolean} True, if and only if the given name is valid group member abbreviation.
+ */
+export const validGroupMemberAbbreviation = ( abbreviation ) => {
+  return (abbreviation.split(/:\s*/g)).every(validAbbreviation);
+}
+
+/**
+ * The default skill type. 
+ * @type {string}
+ */
+export const defaultSkillType = "General";
 
 /**
  * A skill representing a skill group member.
  * @extends {SkillModel}
  */
 export class SkillGroupMember {
-
   /**
    * The group the member belongs to.
+   * @type {SkillGroupModel?}
    */
   #group;
 
@@ -191,6 +216,7 @@ export class SkillGroupMember {
 
   /**
    * The title of the member.
+   * @type {string}
    */
   get title() {
     return this.name();
@@ -198,6 +224,7 @@ export class SkillGroupMember {
 
   /**
    * The abbreviation of the group.
+   * @type {string}
    */
   get abbreviation() {
     return this.#group.abbreviatedMemberName(this.#member);
@@ -205,6 +232,7 @@ export class SkillGroupMember {
 
   /**
    * The description of the group member.
+   * @type {string}
    */
   get description() {
     return this.#member.description;
@@ -212,19 +240,27 @@ export class SkillGroupMember {
 
   /**
    * The group of the group member.
+   * @type {SkillGroupModel?}
    */
   get group() {
     return this.#group;
+  }
+
+  /**
+   * Get skill types.
+   */
+  get skillTypes() {
+    return this.#member.skillTypes || this.#group.skillTypes || [defaultSkillType];
   }
 }
 
 /**
  * Create a skill group.
- * @param {string} groupName The group name.
+ * @param {string?} groupTitle The title of the group.
+ * @param {string} [groupName] The group name. Defaults to the group title
+ * without parentheses.
  * @param {string?} abbreviation The group abbreviation.
- * @param {string?} [description] The group description.  Defaults to no description.
- * @param {string?} [groupTitle] The title of the group. Defaults to no description.
- * @param {ToStringer<SkillModel>?} [composeMemberName] The function generating
+ * @param {string?} [description] The group description.  Defaults to no description. * @param {ToStringer<SkillModel>?} [composeMemberName] The function generating
  * the group member title from the member. Defauls to the
  * function replacing the title generalizer section with member name.
  * @param {ToStringer<SkillModel>?} [abbreviatedMemberName] The function generating
@@ -233,20 +269,24 @@ export class SkillGroupMember {
  * @returns {SkillGroupModel} The skill group model.
  */
 export const createSkillGroup = (
-  groupName,
+  groupTitle,
   abbreviation,
   description = null,
-  groupTitle = null,
+  groupName = null,
   composeMemberName = null,
   abbreviatedMemberName = null
 ) => {
-  if (!validSkillGroupName(groupName)) {
-    throw new TypeError("Invalid skill group name");
+  if (!validSkillGroupName(groupTitle)) {
+    throw new TypeError("Invalid skill group title");
   } else if (
-    groupName.length === 0 ||
-    groupName.trim().length !== groupName.length
+    groupTitle.length === 0 ||
+    groupTitle.trim().length !== groupName.length
   ) {
-    throw new RangeError("Invalid skill group name");
+    throw new RangeError("Invalid skill group title");
+  }
+  const actualGroupName = groupName || groupTitle.replaceAll(/\(\)/g, "");
+  if (!validSkillName(actualGroupName)) {
+    throw new RangeError("Invalid group name")
   }
   if (abbreviation) {
     if (!validAbbreviation(abbreviation)) {
@@ -256,31 +296,127 @@ export const createSkillGroup = (
     }
   }
   return {
-    groupName,
+    title: groupTitle,
+    groupName: actualGroupName,
     abbreviation,
     description,
-    title: /** @type {string} */ (groupTitle ? groupTitle : groupName),
     composeMemberName: composeMemberName
       ? composeMemberName
       : /** @type {ToStringer<SkillModel>} */ (skill) => {
           return (
             skill &&
             skill.title &&
-            (groupTitle || groupName).replaceAll(
+            groupTitle.replaceAll(
               /\(\p{Lu}\p{Ll}*\)/gu,
               skill.title
             )
           );
         },
-      abbreviatedMemberName: abbreviatedMemberName
+    abbreviatedMemberName: abbreviatedMemberName
       ? abbreviatedMemberName
       : /** @type {ToStringer<SkillModel>} */ (skill) => {
           return `${this.abbreviation}: ${skill.abbreviation || skill.title}`;
         },
-    createMember(memberName, title=undefined, abbreviation=undefined, description=undefined) {
-      const member = createSkill(memberName, title, abbreviation, description, this);
+    createMember(
+      memberName,
+      title = undefined,
+      abbreviation = undefined,
+      description = undefined
+    ) {
+      const member = createSkill(
+        memberName,
+        title,
+        abbreviation,
+        description,
+        this
+      );
       return new SkillGroupMember(this, member);
     },
   };
 };
 
+/**
+ * Construct an identifier from a valid name.
+ * @type {IdFunction<string, string>}
+ */
+export function idOfName(name, salt = undefined) {
+  if (validName(name)) {
+    const baseId = name.replaceAll(/\s/g, "_");
+    return `${baseId}${salt ? `:$salt` : ""}`;
+  } else {
+    return false;
+  }
+}
+
+/**
+ * A function determining the identifier of a skill models.
+ * @type {IdFunction<string, SkillModel>}
+ */
+export function skillIdFunction(skill, salt = undefined) {
+  return idOfName(skill.name, salt);
+}
+
+
+/**
+ * A reference to a skill.
+ * @typedef {Object} SkillReference
+ * @property {SkillModel} skill The referred skill.
+ * @property {SkillGroupModel} [group] The skill group, if skil has a group.
+ * @property {ToStringer<SkillReference>} [toString] The stringifier of the reference.
+ */
+
+/**
+ * Function generating the identifier of the skill reference.
+ * @type {IdFunction<string, SkillReference>}
+ */
+export function skillReferenceId(skillRef, salt = undefined) {
+  const groupId = skillRef.group || idOfName(skillRef.group.name);
+  const dkillId = idOfName(skillRef.skill.name);
+  const baseId = (groupId ? `${groupId}.` : "").concat(dkillId);
+  if (salt) {
+    return baseId.concat(`:${salt}`);
+  } else {
+    return baseId;
+  }
+}
+
+/**
+ * The default skill reference library.
+ * @type {ReferenceLibrary<string, SkillReference>}
+ */
+export const defaultSkillRefereceLibrary = new ReferenceLibrary(
+  skillReferenceId
+);
+
+/**
+ * Create a reference to the skill.
+ * @param {SkillModel} skill The referred skill.
+ * @param {SkillGroupModel} [group] The group of the skill.
+ * @param {ReferenceLibrary<string, SkillReference>} [referenceLibrary] The identifier
+ * reference library ensuring no two references share identifier.
+ * @return {SkillReference|undefined} The created skill reference or an undefined
+ * value.
+ */
+export function createSkillReference(
+  skill,
+  group = undefined,
+  referenceLibrary = undefined
+) {
+  const id = referenceLibrary
+    ? referenceLibrary.createId({
+        skill: skill,
+        group: group,
+      })
+    : skill instanceof SkillGroupMember || group == null
+    ? skill.title
+    : group.composeMemberName(skill);
+  if (id) {
+    return {
+      skill,
+      group,
+      id,
+    };
+  } else {
+    return undefined;
+  }
+}
