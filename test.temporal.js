2162f5f80464dc6cf2f86f6379b2d0ca5187cfd4
Antti Kautiainen <antti@kautiainen.com>
2024-10-14 04:37:13 +0300

    test: temporal

---
+++ test.temporal.js
@@ -0,0 +1,14 @@
+
+/**
+ * Test module for temporal files.
+ */
+
+import {describe, it} from 'mocha';
+import {expect} from 'chai';
+import {createDay} from "./modules.temporal.js";
+
+describe("Create day", function() {
+    it("Create a simple day", function () {
+        const day = createDay
+    })
+})
