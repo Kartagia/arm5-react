14a48073a265dcbe1ed7d3fc740254c83613a6c6
Antti Kautiainen <antti@kautiainen.com>
2024-10-17 06:32:46 +0300

    feat: batch program module index.mjs - Changes to be committed: -	new file:   testkit/index.mjs   - feat: main batch program     - feat: load environment.

---
+++ testkit/index.mjs
@@ -0,0 +1,9 @@
+
+/**
+ * The module running the batch development environment without starting the server.
+ */
+import 'dotenv/config';
+
+console.log("Development environment loaded");
+
+console.log("Exiting development console");
