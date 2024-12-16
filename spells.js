a6c1255005b204d68727564f5a466214d2b23d72
Antti Kautiainen <antti@kautiainen.com>
2024-12-16 21:43:00 +0200

    feat: fetch spell guideline sfrom JSOn files
    Changes to be committed:
    	modified:   spells.js
        - Convert tree model JSON guidelines into list.

--- spells.js
+++ spells.js
@@ -3,18 +3,33 @@ import ReactDOM from 'react-dom';
 
 import Spells from './Spells.jsx';
 import SpellView from './SpellView.jsx';
-import { fetchGuidelines } from './modules.spellguidelines.js';
 const rootNode = document.getElementById("react-app");
-fetchGuidelines("./guidelines.txt").then(
+fetch("./guidelines.json").then(
+    (response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error(`Response status: ${response.status}`);
+        }
+    }
+).then(
     (guidelines) => {
         console.log("Guidelines read");
+        const guidelineList = Object.getOwnPropertyNames(guidelines).reduce( (result, form) => (Object.getOwnPropertyNames(guidelines[form]).reduce(
+            (list, tech) => {
+                return Object.getOwnPropertyNames(guidelines[form][tech]).reduce( (values, level) => {
+                    return [...values, ...(guidelines[form][tech][level])]
+                }, list)
+            }, result)), []);
+        
+
         if ("createRoot" in ReactDOM) {
             console.log("React 18 rendering")
             const root = createRoot(rootNode);
-            root.render(<Spells guidelines={guidelines}/>);
+            root.render(<Spells guidelines={guidelineList}/>);
         } else {
             console.log("React 17 rendering")
-            ReactDOM.render(<Spells guidelines={guidelines}/>, rootNode)
+            ReactDOM.render(<Spells guidelines={guidelineList}/>, rootNode)
         }        
     },
     (error) =>{
