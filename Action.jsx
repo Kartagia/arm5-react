8eb76353ea4d076542e63b57af182542399bf6ac
Antti Kautiainen <antti@kautiainen.com>
2025-02-14 21:44:29 +0200

    fix: Action div missing v.

--- Action.jsx
+++ Action.jsx
@@ -1,4 +1,5 @@
 
+import { log } from 'console';
 import React from 'react';
 
 
@@ -32,29 +33,41 @@ export default function Action(props) {
 
     const handleClick = (e) => {
         if (!props.disabled) {
+            log("Handling click");
             if (props.onClick) {
                 props.onClick(e);
+                log("Done onClick");
             }
             if (props.editAction && !props.readonly) {
                 const newValue = props.editAction(props.name, props.value);
+                log("Done editAction with value: " + props.value);
                 if (props.action) {
                     props.action(props.name, newValue);
+                    log("Done action with value: " + newValue);
                 }
             } else {
                 if (props.action) {
                     props.action(props.name, props.value);
+                    log("Done action with value: " + props.value);
                 }
 
             }
+        } else {
+            log("Action disabled");
         }
     }
     const classNames = (typeof props.className === "string" ? props.className.split(/\s+/) : props.className || []);
     if (props.styles) {
         ["diabled", "readonly", "horizontal"].forEach( propName => {
-            if (propName in props && props[propName] && propName in props.styles) {
+            if (propName in props) {
+                log("Adding class for " + propName);
+            }
+            if (propName in props && typeof props[propName] === "boolean" && propName in props.styles) {
                 const addedClasses = typeof props.styles[propName] === "string" ? props.styles[propName].split(/\s+/) : props.styles[propName];
+                log(`Class candidates classes: [${addedClasses.join(",")}]`);
                 addedClasses.forEach( addedClass => {
                     if (!addedClass in classNames) {
+                        log(`Adding class ${addedClass}`);
                         classNames.push(addedClass);
                     }
                 });
@@ -67,8 +80,8 @@ export default function Action(props) {
         return props.imgSrc ? <img className={(classNames.join(" "))} src={props.imgSrc} alt={props.caption || props.name} onClick={handleClick} /> : 
         <span className={(classNames.join(" "))} onClick={handleClick}>[{props.caption || props.name}]</span>
     } else {
-        return <di className={(classNames.join(" "))}v>{props.imgSrc ?
+        return <div className={(classNames.join(" "))}v>{props.imgSrc ?
         <img className={(classNames.join(" "))} src={props.imgSrc} alt={props.caption || props.name} onClick={handleClick} /> :
-        <span className={(classNames.join(" "))} onClick={handleClick}>[{props.caption || props.name}]</span>}</di>
+        <span className={(classNames.join(" "))} onClick={handleClick}>[{props.caption || props.name}]</span>}</div>
     }
 }
