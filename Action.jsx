42d7dff8be96450691c74870fc5d20e28907fe29
Antti Kautiainen <antti@kautiainen.com>
2025-02-14 10:45:55 +0200

    feat: Action property editAction and readonly
    Changes to be committed:
    	modified:   Action.jsx
        - feat: property readonly to determine whether the action is
        read-only.
        - feat: property editAction performing value alteration on click
        before submitting the value to action, if and only if the action
        is not read-only.

--- Action.jsx
+++ Action.jsx
@@ -13,6 +13,11 @@ import React from 'react';
  * @property {URL} [iconSrc] The icon source URL, if the action has icon.
  * @property {EventListener} [onClick] The event listener triggering on the action.
  * @property {(name : string, value?: TYPE) => Void} [action] The action triggered on action.
+ * @property {(name : string, value?: TYPE) => TYPE} [editAction] The edit action triggered on action. The edit
+ * is performed before action, and the resulting value is sent as the action.
+ * @property {boolean} [disabled=false] Is the action disabled.
+ * @property {boolean} [readonly=false] Is the component read-only. Read-only disables all modification
+ * actions.
  */
 
 /**
@@ -23,11 +28,21 @@ import React from 'react';
 export default function Action(props) {
 
     const handleClick = (e) => {
-        if (props.onClick) {
-            props.onClick(e);
-        }
-        if (props.action) {
-            props.action(props.name, props.value);
+        if (!props.disabled) {
+            if (props.onClick) {
+                props.onClick(e);
+            }
+            if (props.editAction && !props.readonly) {
+                const newValue = props.editAction(props.name, props.value);
+                if (props.action) {
+                    props.action(props.name, newValue);
+                }
+            } else {
+                if (props.action) {
+                    props.action(props.name, props.value);
+                }
+
+            }
         }
     }
 
