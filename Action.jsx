ef50123a0a0acc349c4b24cedb00dda9230a6da1
Antti Kautiainen <antti@kautiainen.com>
2025-02-14 21:45:55 +0200

    fix: remove debug messages.
    Changes to be committed:
    	modified:   Action.jsx
        - fix: Remove debug messages.

--- Action.jsx
+++ Action.jsx
@@ -33,41 +33,32 @@ export default function Action(props) {
 
     const handleClick = (e) => {
         if (!props.disabled) {
-            log("Handling click");
             if (props.onClick) {
                 props.onClick(e);
-                log("Done onClick");
             }
             if (props.editAction && !props.readonly) {
                 const newValue = props.editAction(props.name, props.value);
-                log("Done editAction with value: " + props.value);
                 if (props.action) {
                     props.action(props.name, newValue);
-                    log("Done action with value: " + newValue);
                 }
             } else {
                 if (props.action) {
                     props.action(props.name, props.value);
-                    log("Done action with value: " + props.value);
                 }
 
             }
         } else {
-            log("Action disabled");
         }
     }
     const classNames = (typeof props.className === "string" ? props.className.split(/\s+/) : props.className || []);
     if (props.styles) {
         ["diabled", "readonly", "horizontal"].forEach( propName => {
             if (propName in props) {
-                log("Adding class for " + propName);
             }
             if (propName in props && typeof props[propName] === "boolean" && propName in props.styles) {
                 const addedClasses = typeof props.styles[propName] === "string" ? props.styles[propName].split(/\s+/) : props.styles[propName];
-                log(`Class candidates classes: [${addedClasses.join(",")}]`);
                 addedClasses.forEach( addedClass => {
                     if (!addedClass in classNames) {
-                        log(`Adding class ${addedClass}`);
                         classNames.push(addedClass);
                     }
                 });
